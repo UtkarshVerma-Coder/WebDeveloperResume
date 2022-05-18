6c153aec3e570a7e5a8e11ef71c38811b362fe1c
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2022-05-19 02:50:14 +0530

    Added about page

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function home() {
+    document.getElementById("maincont").style.display = 'block';
+    document.getElementById("aboutpage").style.display = 'none';
+    
+}
+
+
+
+function about() {
+    document.getElementById("maincont").style.display = 'none';
+    document.getElementById("aboutpage").style.display = 'block';
+}
